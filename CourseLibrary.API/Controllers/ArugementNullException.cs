23fcfd66806dd8322ce0c75d7afc91d3a5dc5ac8
Zia Rehman <zia.rehman@scanmarqed.com>
2022-01-24 14:01:28 +0000

    Add project files.

---
+++ CourseLibrary.API/Controllers/ArugementNullException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CourseLibrary.API.Controllers
+{
+    [Serializable]
+    internal class ArugementNullException : Exception
+    {
+        public ArugementNullException()
+        {
+        }
+
+        public ArugementNullException(string message) : base(message)
+        {
+        }
+
+        public ArugementNullException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ArugementNullException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ CourseLibrary.API/Controllers/AuthorsControllers.cs
@@ -0,0 +1,24 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    public class AuthorsControllers: ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsControllers(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArugementNullException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet("api/authors")]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+
+    }
+}
